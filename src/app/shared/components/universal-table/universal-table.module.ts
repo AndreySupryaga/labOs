84dd5c2999791c3cb16edff50c54343863125be3
Andrey Supryaga <asupryaga@datamola.com>
2023-03-30 00:26:23 +0300

    Added fav icon

--- src/app/shared/components/universal-table/universal-table.module.ts
+++ src/app/shared/components/universal-table/universal-table.module.ts
@@ -3,6 +3,7 @@ import {CommonModule} from '@angular/common';
 import {UniversalTableComponent} from '@shared/components/universal-table/universal-table.component';
 import {MatTableModule} from '@angular/material/table';
 import {TranslateModule} from '@ngx-translate/core';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
 	declarations: [UniversalTableComponent],
@@ -12,7 +13,8 @@ import {TranslateModule} from '@ngx-translate/core';
 	imports: [
 		CommonModule,
 		MatTableModule,
-		TranslateModule
+		TranslateModule,
+		MatIconModule
 	]
 })
 export class UniversalTableModule {
