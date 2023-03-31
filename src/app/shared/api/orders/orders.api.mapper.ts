4edbca4dfd2f7e739867ae0041859ff0cdd5fbe8
Andrey Supryaga <asupryaga@datamola.com>
2023-03-31 09:52:33 +0300

    Deleted unnecessary @Injector from api mapper.

--- src/app/shared/api/orders/orders.api.mapper.ts
+++ src/app/shared/api/orders/orders.api.mapper.ts
@@ -1,7 +1,5 @@
-import {Injectable} from '@angular/core';
 import {Order, OrderApiResponse} from '@entities/orders/model';
 
-@Injectable()
 export class OrdersApiMapper {
 	public static mapData(data: OrderApiResponse): Order[] {
 		return data.order
--- src/app/shared/api/patients/patients.api.mapper.ts
+++ src/app/shared/api/patients/patients.api.mapper.ts
@@ -1,7 +1,5 @@
-import {Injectable} from '@angular/core';
 import {Patient, PatientsApiResponse} from '@entities/patients/model';
 
-@Injectable()
 export class PatientsApiMapper {
 	public static mapData(data: PatientsApiResponse): Patient[] {
 		return data.patient
